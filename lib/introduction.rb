a00c101b5b53a748e5e304818e96601b81af3e39
Juan Soto <jasotojr@gmail.com>
2019-09-19 02:16:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,11 +10,11 @@ end
 
 introduction_with_language("Juano", "Ruby")
 
-def introduction_with_language_optional(name, language = Ruby)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Juano", "Ruby")
+introduction_with_language("Juano", "Javascript")
 
 
 
